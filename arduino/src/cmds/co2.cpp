5539b4d2801462f4b1a4ebb557b4da316a46940f
tiangel1 <95332815+tiangel1@users.noreply.github.com>
2024-05-22 04:29:26 -0700

    minor debug msg add

--- arduino/src/cmds/co2.cpp
+++ arduino/src/cmds/co2.cpp
@@ -85,12 +85,14 @@ void co2_init()
 {
     // Iterate through the 8 sensor multiplexed channels
     // to begin config files.
-    for (int i = 0; i < 8; ++i) {
-        mux_set_channel(i);
+    for (int i = 0; i < 1; ++i) {
+        //mux_set_channel(i);
         if (scd30.begin()) {
             // Enable continuous measurement, where we can just query for data,
             // not needing to request it each time.
-            scd30.startContinuousMeasurement();
+            if(scd30.setMeasurementInterval(1)) {
+                DEBUG_LOG("SCD30 continuous measurement fault");
+            }
         } else {
             DEBUG_LOG("Failed to initialize SCD30 sensor (channel = %d)", i);
         }
