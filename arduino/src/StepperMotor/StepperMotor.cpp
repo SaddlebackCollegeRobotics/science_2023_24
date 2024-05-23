7614870beedf7139c30c82069e65610d5a6cc92f
Matthew <awesomepop012@hotmail.com>
2024-05-22 20:29:37 -0700

    Update stepperMotor.

--- arduino/src/StepperMotor/StepperMotor.cpp
+++ arduino/src/StepperMotor/StepperMotor.cpp
@@ -2,17 +2,61 @@
 
 #include "Arduino.h"
 #include "pin_defines.hpp"
-#include <arduino-timer.h>
+#include <TimerOne.h>
+
+namespace {
+
+constexpr auto NUM_STEPPERS = 4;
+
+struct stepper_info
+{
+    bool enabled = false;
+    bool state = LOW;
+    pins::pin_t pin = 0;
+};
+
+util::array<stepper_info, NUM_STEPPERS> steppers_info{};
+
+void stepper_pwn_callback()
+{
+
+    for (auto& stepper : steppers_info) {
+        // Pin 0 denotes uninitialized stepper
+        if (stepper.pin == 0) {
+            continue;
+        }
+
+        if (!stepper.enabled) {
+            continue;
+        }
+
+        DEBUG_LOG("Stepping motor %d (state = %s)", stepper.pin, stepper.state ? "HIGH" : "LOW");
+
+        digitalWrite(stepper.pin, stepper.state ? HIGH : LOW);
+        stepper.state = !stepper.state;
+    }
+}
+
+} // namespace
 
 StepperMotor::StepperMotor(pins::pin_t dir_pin, pins::pin_t step_pin, uint32_t step_period_millis)
     : dir_pin_(dir_pin)
     , step_pin_(step_pin)
     , step_period_millis_(step_period_millis)
+    , id_(s_idx++)
 {
+    steppers_info[id_] = {.enabled = false, .state = LOW, .pin = step_pin};
     pinMode(dir_pin, OUTPUT);
     pinMode(step_pin, OUTPUT);
 
     setDirection(dir_);
+
+    DEBUG_ASSERT(id_ < NUM_STEPPERS, "Too many steppers instantiated! (%d > %d)", id_, NUM_STEPPERS - 1);
+    if (!s_setup_interrupt) {
+        s_setup_interrupt = true;
+        // Assume Timer1 is already initialized
+        Timer1.attachInterrupt(stepper_pwn_callback);
+    }
 }
 
 void StepperMotor::setDirection(Direction dir)
@@ -29,23 +73,12 @@ void StepperMotor::setDirection(Direction dir)
     }
 }
 
-// Sets direfction
-void StepperMotor::setRunState(bool state)
+void StepperMotor::start()
 {
-    is_running_ = state;
+    steppers_info[id_].enabled = true;
 }
 
-void StepperMotor::update()
+void StepperMotor::stop()
 {
-    if (!is_running_) {
-        return;
-    }
-
-    if (millis() - last_update_millis_ >= step_period_millis_) {
-        DEBUG_LOG("Stepping motor (%d)", static_cast<int>(last_write_mode_));
-        last_update_millis_ = millis();
-
-        last_write_mode_ = !last_write_mode_;
-        digitalWrite(step_pin_, static_cast<uint8_t>(last_write_mode_));
-    }
+    steppers_info[id_].enabled = false;
 }
--- arduino/src/StepperMotor/StepperMotor.hpp
+++ arduino/src/StepperMotor/StepperMotor.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "pin_defines.hpp"
-#include <arduino-timer.h>
+#include <Arduino.h>
 
 // Simple class for the usongshine stepping motor (model #????)
 class StepperMotor
@@ -13,11 +13,12 @@ public:
         NEGATIVE
     };
 
-    StepperMotor(pins::pin_t dir_pin, pins::pin_t step_pin, uint32_t step_period_millis = 10);
+    StepperMotor(pins::pin_t dir_pin, pins::pin_t step_pin, uint32_t step_period_millis = 2);
 
     void setStepPeriod(uint32_t step_period_millis) { step_period_millis_ = step_period_millis; }
     void setDirection(Direction dir);
-    void setRunState(bool state);
+    void start();
+    void stop();
 
     void update();
 
@@ -28,7 +29,8 @@ private:
     uint32_t step_period_millis_ = 10;
     Direction dir_ = Direction::POSITIVE;
 
-    unsigned long last_update_millis_ = 0;
-    bool last_write_mode_ = LOW;
-    bool is_running_ = false;
+    uint8_t id_;
+
+    inline static bool s_setup_interrupt = false;
+    inline static uint8_t s_idx = 0;
 };
