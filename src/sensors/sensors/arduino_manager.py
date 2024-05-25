f70d3f5e863ba96b03d4cd6b63092558f3a5472e
Matthew Reese <awesomepop012@hotmail.com>
2024-05-25 10:36:47 -0700

    Add tof querying to arduino publisher.

--- src/sensors/sensors/arduino_manager.py
+++ src/sensors/sensors/arduino_manager.py
@@ -1,6 +1,6 @@
 from threading import Thread
 from time import sleep
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray, UInt16
 from rcl_interfaces.srv import DescribeParameters
 
 import rclpy
@@ -25,26 +25,31 @@ class ArduinoManager(Node):
             "co2_7": self.create_publisher(Float32MultiArray, "/co2_sensor_7", 10),
             "co2_8": self.create_publisher(Float32MultiArray, "/co2_sensor_8", 10),
         }
+        self.tof_sensor_publishers = {
+            "tof_1": self.create_publisher(UInt16, "/tof_sensor_1", 10),
+            "tof_2": self.create_publisher(UInt16, "/tof_sensor_2", 10),
+        }
 
         self.cli = self.create_client(DescribeParameters, "science_rpc")
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("ScienceRPC service not available, waiting again...")
 
-        self.msg = Float32MultiArray()
+        self.co2_msg = Float32MultiArray()
+        self.tof_msg = UInt16()
 
-    def send_data_requests(self):
+    def send_co2_data_requests(self):
         while True:
             for sens_dev, pub in self.co2_sensor_publishers.items():
-                self.msg.data = [0.0] * 3
+                self.co2_msg.data = [0.0] * 3
                 try:
-                    self.msg.data[0] = float(
+                    self.co2_msg.data[0] = float(
                         self.cli.call(
                             DescribeParameters.Request(names=[sens_dev, "read_co2", ""])
                         )
                         .descriptors[0]
                         .name
                     )
-                    self.msg.data[1] = float(
+                    self.co2_msg.data[1] = float(
                         self.cli.call(
                             DescribeParameters.Request(
                                 names=[sens_dev, "read_temp", ""]
@@ -53,7 +58,7 @@ class ArduinoManager(Node):
                         .descriptors[0]
                         .name
                     )
-                    self.msg.data[2] = float(
+                    self.co2_msg.data[2] = float(
                         self.cli.call(
                             DescribeParameters.Request(
                                 names=[sens_dev, "read_humid", ""]
@@ -62,10 +67,36 @@ class ArduinoManager(Node):
                         .descriptors[0]
                         .name
                     )
-                    pub.publish(self.msg)
+                    pub.publish(self.co2_msg)
                 except TypeError as e:
                     self.get_logger().warn(f"Invalid cmd response ({e})")
                     break
+                except Exception as e:
+                    self.get_logger().warn(
+                        f"Unhandled exception type when getting co2 data! ({e})"
+                    )
+            sleep(0.25)
+
+    def send_tof_data_requests(self):
+        while True:
+            for sens_dev, pub in self.tof_sensor_publishers.items():
+                self.tof_msg.data = 0
+                try:
+                    self.tof_msg.data = int(
+                        self.cli.call(
+                            DescribeParameters.Request(names=[sens_dev, "read", ""])
+                        )
+                        .descriptors[0]
+                        .name
+                    )
+                    pub.publish(self.tof_msg)
+                except TypeError as e:
+                    self.get_logger().warn(f"Invalid cmd response ({e})")
+                    break
+                except Exception as e:
+                    self.get_logger().warn(
+                        f"Unhandled exception type when getting tof data! ({e})"
+                    )
             sleep(2)
 
 
@@ -78,7 +109,9 @@ def main(args=None):
     spin_thread.start()
 
     try:
-        arduino_manager.send_data_requests()
+        co2_req_thread = Thread(target=arduino_manager.send_co2_data_requests)
+        co2_req_thread.start()
+        arduino_manager.send_tof_data_requests()
     except KeyboardInterrupt:
         print("Exiting...")
 
