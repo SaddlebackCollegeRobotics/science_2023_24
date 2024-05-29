7fd37c05daa32605f9d3f03c358bec68e1cccd82
Matthew Reese <awesomepop012@hotmail.com>
2024-05-28 20:17:25 -0700

    Added serial.close() to service class dtor.

--- src/py_srvcli/py_srvcli/service_member_function.py
+++ src/py_srvcli/py_srvcli/service_member_function.py
@@ -18,7 +18,10 @@ class ScienceServer(Node):
             DescribeParameters, "science_rpc", self.science_rpc_callback
         )
 
-        self._arduino_serial = Serial("/dev/ttyACM1", 9600, timeout=1)
+        self._arduino_serial = Serial("/dev/ttyACM0", 9600, timeout=1)
+
+    def __del__(self):
+        self._arduino_serial.close()
 
     def science_rpc_callback(self, request, response):
         command_str = ",".join(request.names) + ","
